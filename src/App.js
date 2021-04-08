318b66302c5aa94c6f6433d0c9202efe74cf55dc
mubariz <mhdmdssr999@gmail.com>
2021-04-09 05:00:28 +0530

    functionality to login

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-
 import { ChatEngine } from 'react-chat-engine';
 
 import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
 import './App.css';
 
+const projectID = '37993ead-44d2-4e32-b337-0980831eb5fb';
 
 const App = () => {
+  if (!localStorage.getItem('username')) return <LoginForm />;
 
   return (
     <ChatEngine
       height="100vh"
-      projectID="37993ead-44d2-4e32-b337-0980831eb5fb"
-      userName="mubariz"
-      userSecret="1234"
+      projectID={projectID}
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
       renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
     />
   );
 };
-
 
 export default App;
